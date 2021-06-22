6ba905083a00736deeeb4f26f6fd9c9be89f683a
DickKemp <dickkemp14@gmail.com>
2021-06-22 11:20:51 -0400

    refactoring

---
+++ api/route/home.py
@@ -0,0 +1,40 @@
+from http import HTTPStatus
+from flask import current_app
+from flask import Blueprint
+from flasgger import swag_from
+from api.schema.hello import HelloSchema
+from api.model.hello import HelloModel
+
+home_api = Blueprint('api', __name__)
+
+@home_api.route('/')
+@swag_from({
+    'responses': {
+        HTTPStatus.OK.value: {
+            'description': 'Welcome to the Flask Starter Kit',
+            'schema': HelloSchema
+        }
+    }
+})
+def welcome():
+    """
+    1 liner about the route
+    A more detailed description of the endpoint
+    ---
+    """
+    result = HelloModel()
+    return HelloSchema().dump(result), 200
+
+
+@home_api.route('/hello')
+def hello():
+    return 'Hello, World!'
+
+@home_api.route('/config')
+def config():
+    ret = f'Hey.  Env: {current_app.config["TEST_ENV_VARIABLE"]}'
+    return ret
+
+@home_api.route('/goodbye')
+def goodbuy():
+    return 'Goodbye cruel world!'
---
+++ appfactory.py
@@ -0,0 +1,34 @@
+import os
+from flask import Flask
+from flasgger import Swagger
+from api.route.home import home_api
+
+def create_app(test_config=None):
+
+    app = Flask(__name__)
+
+    app.config['SWAGGER'] = {
+        'title': 'The Rich API'
+    }
+    
+    app.config["TEST_ENV_VARIABLE"] = os.environ['TEST_ENV_VARIABLE']
+
+    swagger = Swagger(app)
+
+    app.register_blueprint(home_api, url_prefix='/api')
+
+    return app
+
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser()
+    parser.add_argument('-p', '--port', default=5000, type=int, help='port to listen on')
+    args = parser.parse_args()
+    port = args.port
+
+    app = create_app()
+
+    app.run(port=port)
+
