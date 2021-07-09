7a47fc0e28a47c3d3e69ab77b4ebf59af064a872
DickKemp <dickkemp14@gmail.com>
2021-07-09 12:31:34 -0400

    render svg

--- apis/__init__.py
+++ apis/__init__.py
@@ -2,6 +2,7 @@ from flask_restx import Api
 
 from .namespace1 import api as ns1
 from .namespace2 import api as ns2
+from .blocks import api as blocks
 
 api = Api(
     title='My Title',
@@ -12,3 +13,4 @@ api = Api(
 
 api.add_namespace(ns1)
 api.add_namespace(ns2)
+api.add_namespace(blocks)
